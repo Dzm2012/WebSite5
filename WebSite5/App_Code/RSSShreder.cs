9db64cea323a3c96bdbed224e8d04b97b13dd1d3
dzm2012 <dzm201225@gmail.com>
2016-02-02 11:29:37 -0500

    Added rss logic

---
+++ WebSite5/App_Code/RSSShreder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+/// <summary>
+/// Summary description for RSSShreder
+/// </summary>
+public class RSSShreder
+{
+    public List<FeedDeffinition> items = new List<FeedDeffinition>();
+    public RSSShreder(string rssfeedUrl)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(rssfeedUrl);
+        XmlNodeList nodes = doc.SelectNodes(@"//item");
+        foreach(XmlNode node in nodes)
+        {
+            XmlDocument reader = new XmlDocument();
+            FeedDeffinition feed = new FeedDeffinition();
+
+            reader.LoadXml("<root>" + node.InnerXml + "</root>");
+
+            feed.Title = reader.SelectSingleNode("//title").InnerText;
+            feed.Link = reader.SelectSingleNode("//link").InnerText;
+            feed.Description = reader.SelectSingleNode("//description").InnerText;
+            items.Add(feed);
+        }
+    }
+}
--- WebSite5/Default.aspx.cs
+++ WebSite5/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -10,11 +11,19 @@ public partial class _Default : System.Web.UI.Page
     public List<string> latin = new List<string>();
     protected void Page_Load(object sender, EventArgs e)
     {
-        
-        latin.Add("Lorem ipsum dolor sit amet, at mea veniam tantas accumsan. Quo id dicant epicuri comprehensam, velit nominavi epicurei ex mea. Ne pro libris insolens, meis eleifend salutandi eu mei, tation vidisse pro ad. Luptatum mediocrem consulatu mea et, ei eros iriure vix. Eam percipit principes expetendis id, iudico ullamcorper his cu.");
-        latin.Add("Ad est modo mundi percipitur, iracundia constituto ex vel, per an debitis reprimique. Per ad forensibus neglegentur, ius ad tritani minimum. Sed cu illud praesent, his ad quod doming, simul partem consequat ex vel. An mea magna dictas nonumes, pro quod dicit expetenda ne. Eos omnium maiestatis ei, agam ornatus explicari cum ad. Liber mundi aliquid ad vim, alienum splendide deterruisset ea duo. Urbanitas disputationi in sed, cu sonet rationibus mei, quo cu fierent albucius adversarium.");
-        latin.Add("Eum unum deserunt theophrastus ea. Ea vim augue semper utamur, cu iuvaret definitiones has. Ne mea porro legimus tacimates, nam nulla quando pertinax at. Veniam definiebas mei et. Sed et vide rebum vivendum, nam enim dissentias no. Id usu ipsum malis scripserit.");
-        latin.Add("Quod quas cu cum, ex bonorum forensibus sed. Vix id assum alterum perpetua, quo justo ubique eu. Eos detracto consulatu neglegentur in. Ad quot error delenit qui, pri no cetero molestie iudicabit. Cum agam persius eu, meis regione propriae qui ea.");
-
+        List<RSSShreder> Feeds = new List<RSSShreder>();
+        Feeds.Add(new RSSShreder(@"https://www.realwire.com/rss/?id=183&row=&view=Synopsis"));
+        Feeds.Add(new RSSShreder(@"https://www.realwire.com/rss/?id=184&row=&view=Synopsis"));
+        foreach (var Feed in Feeds)
+        {
+            foreach (var item in Feed.items)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" <h2>" + item.Title + "</h2> <br />");
+                sb.Append(item.Description + "<br /><br />");
+                sb.Append("<a href = \"" + item.Link + "\" >" + item.Link + "</a><br /><br />");
+                latin.Add(sb.ToString());
+            }
+        }
     }
 }
